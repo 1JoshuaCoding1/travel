fe92034c6a52319937eb62934b0b424f11f35912
aldione <aldionejoshua.pancho.21@usjr.edu.ph>
2023-12-04 20:44:42 +0800

    add commponents

---
+++ src/app/booked-flights/booked-flights.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booked-flights',
+  templateUrl: './booked-flights.component.html',
+  styleUrls: ['./booked-flights.component.css']
+})
+export class BookedFlightsComponent {
+
+}
