8c6b9ab5596983bcdbb38922acf49f4814a8fb0a
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2023-12-07 16:45:12 +0800

    Transferred all files to here

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { setMapboxAccessToken } from './mapbox-config';
+import * as mapboxgl from 'mapbox-gl';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './map.component.html',
+  styleUrls:[ './map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  ngOnInit(): void {
+    const map = new mapboxgl.Map({
+      accessToken: 'pk.eyJ1IjoiaXprdW1iZXJkdSIsImEiOiJjbHA1ZXV6d3kxbXY1Mm9xdWM3Nmg3bG1nIn0.kEkGXezKiCHmcwVgTHpqOA',
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+  }
+}
---
+++ src/app/view-flights/view-flights.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-flights',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-flights.component.html',
+  styleUrl: './view-flights.component.css'
+})
+export class ViewFlightsComponent {
+
+}
